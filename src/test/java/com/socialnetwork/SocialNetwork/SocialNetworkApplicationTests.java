1ba6b0d1261cd341cbe6cffa8dd3da1dcdfa7017
CristianArenas <cdarenas02@gmail.com>
2023-12-20 19:02:29 -0500

    Creacion de proyecto

---
+++ src/test/java/com/socialnetwork/SocialNetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialnetwork.SocialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
